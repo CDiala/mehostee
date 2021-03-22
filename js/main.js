8562440dbb8a972f74e277b9e64b99270e74d758
Chibuzo Diala <dialachibuzo@yahoo.com>
2021-03-22 06:59:38 +0100

    Created nav component, modified main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
-  })
-);
+// const collapsibles = document.querySelectorAll(".collapsible");
+// collapsibles.forEach((item) =>
+// item.addEventListener("click", function () {
+//   this.classList.toggle("collapsible--expanded");
+// })
+// );
+
+// const collapseIcon = document.querySelector(".collapsible__arrow");
+// console.log(collapseIcon);
+// console.log(collapseIcon.parentElement.classList);
+// collapseIcon.addEventListener("click", function() {
+//   collapseIcon.parentElement.classList.toggle("collapsible--expanded");
+// })
+
+const navMenuCollapsed = document.querySelector(".nav__menu");
+// console.log(navMenuCollapsed);
+// console.log(navMenuCollapsed.parentElement.classList);
+navMenuCollapsed.addEventListener("click", function() {
+  navMenuCollapsed.parentElement.classList.toggle("collapsible--expanded");
+})
